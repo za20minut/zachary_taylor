b50f098e36b8a1b8db2680312408aca2501cbe9c
za20minut <mateuszm3442@gmail.com>
2024-05-19 17:47:46 +0200

    Zjazd P

--- wodrow_wilson.cpp
+++ wodrow_wilson.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <functional>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+
+
 
 class miasto{
     private:
@@ -13,6 +19,9 @@ class miasto{
     public:
    string returnMiasta(){
     return nazwa_miasta;
+   }
+   int returnMieszkancy(){
+    return mieszkancy;
    }
     miasto(string a, int b, string c,string d){
         this->nazwa_miasta = a;
@@ -30,6 +39,7 @@ class miasto{
         cout<<this->nazwa_miasta<<" ";
         
     }
+
 
     void modyfikacja(string g,int h){
 
@@ -57,7 +67,7 @@ class panstwo{
          cout<<this->nazwa_panstwa<<endl;
     }
 };
-
+panstwo* currentDir=nullptr;
 class kontynent {
    
 
@@ -72,22 +82,86 @@ class kontynent {
    }
 
 
-   vector<panstwo> panstwa;
+   vector<panstwo*> panstwa;
+
+   void printKontynent(){
+    cout<<this->nazwa_kontynentu<<endl;
+   }
 };
 
 
+void changeDir(kontynent* &a,panstwo* &b) {
+    string input, input2;
+    
+    cout << "podaj nazwe panstwa\n";
+    cin >> input;
+    
+    for(auto kraj : a->panstwa) {
+        if(kraj->returnNazwaPanstwa() == input) {
+           b = kraj;
+            return;
+        }
+    }
+}
+
+void PolskiChangerDir(kontynent* &a,panstwo* &b){
+   string input, input2;
+    
+   
+     input="Polska";
+    
+    for(auto kraj : a->panstwa) {
+        if(kraj->returnNazwaPanstwa() == input) {
+           b = kraj;
+            return;
+        }
+    } 
+}
+
+void LitewskiChangerDir(kontynent* &a,panstwo* &b){
+   string input, input2;
+    
+   
+     input="Litwa";
+    
+    for(auto kraj : a->panstwa) {
+        if(kraj->returnNazwaPanstwa() == input) {
+           b = kraj;
+            return;
+            
+        }
+    } 
+}
 
 
 
-void dodawanie(){
+
+void LotewskiChangerDir(kontynent* &a,panstwo* &b){
+   string input, input2;
+    
+   
+     input="Lotwa";
+    
+    for(auto kraj : a->panstwa) {
+        if(kraj->returnNazwaPanstwa() == input) {
+           b = kraj;
+            return;
+        }
+    } 
+}
+
+
+
+
+void dodawanie(kontynent* &a, panstwo* &b){
     string nazwa;
     int liczba;
 cout<<"Nazwa miasta\n";
 cin>>nazwa;
 cout<<"liczba mieszkancow\n";
 cin>>liczba;
-temp = new miasto(nazwa, liczba,kraj->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
+miasto* temp = new miasto(nazwa, liczba,b->returnNazwaPanstwa(),a->returnKontynent());
+    b->miasta.emplace_back(temp);
 
    
 
@@ -97,16 +171,16 @@ temp = new miasto(nazwa, liczba,kraj->returnNazwaPanstwa(),kont->returnKontynent
 
 
 
-void usuwanie(){
+void usuwanie( panstwo* &b){
 
 string usumiasto;
  cout << "Jakie miasto usunac\n";
     cin>> usumiasto;
   int g = 0;
-    while(g < kraj->miasta.size()){
-        if(kraj->miasta[g]->returnMiasta() == usumiasto){
+    while(g < b->miasta.size()){
+        if(b->miasta[g]->returnMiasta() == usumiasto){
 
-            kraj->miasta.erase(kraj->miasta.begin()+g);
+            b->miasta.erase(b->miasta.begin()+g);
             
             
             
@@ -119,7 +193,7 @@ string usumiasto;
 
 
 
-void modyfikacja(){
+void modyfikacja( panstwo* &b){
 string mod;
 cout<<"ktore zmodyfikowac?"<<endl;
     cin>>mod;
@@ -130,11 +204,11 @@ cout<<"ktore zmodyfikowac?"<<endl;
     cin>>modmia;
      cout<<"mieszkancy"<<endl;
     cin>>modmie;
-    while(q < kraj->miasta.size()){
+    while(q < b->miasta.size()){
        
-        if(kraj->miasta[q]->returnMiasta() == mod){
+        if(b->miasta[q]->returnMiasta() == mod){
          
-            kraj->miasta[q]->modyfikacja(modmia,modmie);
+            b->miasta[q]->modyfikacja(modmia,modmie);
            
             break;
             
@@ -146,15 +220,16 @@ cout<<"ktore zmodyfikowac?"<<endl;
 
 
 
-void odczyt(){
+void odczyt(kontynent* &a, panstwo* &b){
     string temp3;
  cout << "Jakie miasto\n";
     cin>> temp3;
   int n = 0;
-    while(n < kraj->miasta.size()){
-        if(kraj->miasta[n]->returnMiasta() == temp3){
-            kraj->miasta[n]->printMiasto();
-            kraj->printPanstwo();
+    while(n < b->miasta.size()){
+        if(b->miasta[n]->returnMiasta() == temp3){
+            b->miasta[n]->printMiasto();
+            b->printPanstwo();
+            a->printKontynent();
             
             
             break;
@@ -167,16 +242,105 @@ void odczyt(){
 
 
 
-void drzewko(){
-    int m =0;
-    while (m<kraj->miasta.size())
-    {
-        kraj->miasta[m]->printMiastoBezEnter();
-        m++;
-    }
+void drzewko(kontynent* &a, panstwo* &b){
+    for(auto  c: a->panstwa) {
+        cout<<endl<<c->returnNazwaPanstwa()<<" ";
+        for (auto  h: c->miasta)
+        {
+            h->printMiastoBezEnter();
+       }
+        
+        
+    } 
 }
+
+void save(kontynent* &a, panstwo* &b){
+
+
+    ofstream f ("plik.txt");
+if (f.is_open())
+{
+   
+for(auto  c: a->panstwa) {
+        
+        for (auto  h: c->miasta)
+        {
+           f<< h->returnMiasta()<<","<<h->returnMieszkancy()<<";";
+       }
+       f<<'\n'; 
+        
+    } 
+
+
+
+
+    f.close();
+}
+
+
+}
+
+
+
+
+
+void load(kontynent* &a, panstwo* &b){
+
+    ifstream inf("plik.txt");
+if (inf.is_open())
+{
+     string temp,temp2,temp3;
+    string line;
+    vector<string> loaded;
+     stringstream ss(line);
+     int u=1;
+     int miesz;
+     string nazwamias;
+while (getline(inf,line,';'))
+{
+    stringstream ss2(temp);
+    while(getline(ss2, temp2, ',')) {
+            loaded.push_back(temp2);
+        }
+}
+for(auto& load : loaded){
+    if (u%2==0)
+    {
+        temp3=temp2;
+        miesz = stoi(temp3);
+        miasto* temp = new miasto(nazwamias, miesz,b->returnNazwaPanstwa(),a->returnKontynent());
+        b->miasta.emplace_back(temp);
+    }
+    else{
+nazwamias=temp2;
+    }
+    u++;
+}
+
+
+
+
+
+   // string nazwa;
+  //  int liczba;
+
+//miasto* temp = new miasto(nazwa, liczba,b->returnNazwaPanstwa(),a->returnKontynent());
+    //b->miasta.emplace_back(temp);
+
+
+}
+
+}
+
+  
+
+
 int main(){
-    kontynent* kont = new kontynent("Europa");
+    
+
+
+
+kontynent* kont = new kontynent("Europa");
     panstwo* kraj = new panstwo("Polska");
     panstwo* kraj2 = new panstwo("Litwa");
     panstwo* kraj3 = new panstwo("Lotwa");
@@ -188,59 +352,88 @@ int main(){
     int temp2;
     string nazwa;
     int liczba;
+    panstwo* cd=new panstwo("");
+
+    PolskiChangerDir(kont,cd);
+    miasto* temp = new miasto("Warszawa", 100000,cd->returnNazwaPanstwa(),kont->returnKontynent());
+    cd->miasta.emplace_back(temp);
+    temp = new miasto("Radom", 1,cd->returnNazwaPanstwa(),kont->returnKontynent());
+    cd->miasta.emplace_back(temp);
+    LitewskiChangerDir(kont,cd);
+    temp = new miasto("Wilno", 600000,cd->returnNazwaPanstwa(),kont->returnKontynent());
+    cd->miasta.emplace_back(temp);
+    LotewskiChangerDir(kont,cd);
+    temp = new miasto("Riga", 450000,cd->returnNazwaPanstwa(),kont->returnKontynent());
+    cd->miasta.emplace_back(temp);
+    temp = new miasto("Dyneburg", 50000,cd->returnNazwaPanstwa(),kont->returnKontynent());
+    cd->miasta.emplace_back(temp);
+
+
     
-    miasto* temp = new miasto("Warszawa", 100000,kraj->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
-   temp = new miasto("Radom", 1,kraj->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
-    temp = new miasto("Wilno", 600000,kraj2->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
-    temp = new miasto("Riga", 450000,kraj3->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
-    temp = new miasto("Dyneburg", 50000,kraj3->returnNazwaPanstwa(),kont->returnKontynent());
-    kraj->miasta.emplace_back(temp);
+   
+   
+    
+  //  for(auto  b: kont->panstwa) {
+   //     cout<<b->returnNazwaPanstwa()<<" "<<endl;
+  //      for (auto  a: b->miasta)
+   //     {
+  //          a->printMiastoBezEnter();
+   //    }
+        
+        
+  //  } 
 
 
 
+changeDir(kont,cd);
 
 
+while (true)
+{
+   
 
 
+string kom;
+cout<<endl<<"save-zapisywanie"<<endl<<"load-wczytywanie"<<endl<<"cd-zmiana wezla"<<endl<<"mo-dodawanie obiektu"<<endl<<"do-usuwanie obiektu"<<endl<<"mdo-modyfikacja obiektu"<<endl<<"show-wyswietlenie informacji"<<endl<<"tree-wyswietlenie drzewka"<<endl<<"0=wyjdz"<<endl;
+cin>>kom;
+if (kom=="load")
+{
+    load(kont,cd);
+}
 
+if (kom=="save")
+{
+    save(kont,cd);
+}
 
+if(kom=="cd"){
+changeDir(kont,cd);
+}
+if (kom=="mo")
+{
+  dodawanie(kont,cd);  
+}
+if (kom=="do")
+{
+    usuwanie(cd);
+}
+if (kom=="mdo")
+{
+    modyfikacja(cd);
+}
+if (kom=="show")
+{
+    odczyt(kont,cd);
+}
+if (kom=="tree")
+{
+    drzewko(kont,cd);
+}
+if(kom=="0"){
+    break;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
     }
